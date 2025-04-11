618762e338099a30d6f43c3773202d053f061fe3
Polina Belash <polinka.zhuravlinka@gmail.com>
2025-04-11 12:09:06 +0300

    feat: add grid styles for calculator buttons

--- src/app/createCalculatorUi.js
+++ src/app/createCalculatorUi.js
@@ -1,24 +1,25 @@
 export const createCalculatorView = () => `<div class="calculator">
                 <output class="calculator__display">0</output>
+                <button class="calculator__button-operation clear">AC</button>
+                <button class="calculator__button-operation sign-change">+/-</button>
+                <button class="calculator__button-operation percent">%</button>
+                <button class="calculator__button-operation divide">/</button>
                 <button class="calculator__button">7</button>
+
                 <button class="calculator__button">8</button>
                 <button class="calculator__button">9</button>
-                <button class="calculator__button-operation">-</button>
-                <button class="calculator__button-operation">/</button>
-
+                <button class="calculator__button-operation multiple">*</button>
                 <button class="calculator__button">4</button>
                 <button class="calculator__button">5</button>
-                <button class="calculator__button">6</button>
-                <button class="calculator__button-operation">+</button>
-                <button class="calculator__button-operation">*</button>
 
+                <button class="calculator__button">6</button>
+                <button class="calculator__button-operation minus">-</button>
                 <button class="calculator__button">1</button>
                 <button class="calculator__button">2</button>
-                <button class="calculator__button">3</button>
-                <button class="calculator__button-operation">%</button>
 
-                <button class="calculator__button">0</button>
+                <button class="calculator__button">3</button>
+                <button class="calculator__button-operation plus">+</button>
+                <button class="calculator__button zero">0</button>
                 <button class="calculator__button">.</button>
-                <button class="calculator__button">C</button>
-                <button class="calculator__button-operation_equals">=</button>
+                <button class="calculator__button-operation equals">=</button>
 </div>`;
